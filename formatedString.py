b4274cefe1b7f435c14ab911e190dc373ec670fb
FarhanZaheerBabar <71889240+FarhanZaheerBabar@users.noreply.github.com>
2020-09-27 17:14:24 +0500

    add python Formated String file

---
+++ formatedString.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number :"))
+
+if n > 0:
+    print(f"the number {n} is positive.")
+elif n < 0:
+    print(f"the number {n} is negative")
+else:
+    print(f"the number {n} is zero")
