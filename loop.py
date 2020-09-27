4c4d57d0d49ca14611a3d04f680f259bd8f9f9c0
FarhanZaheerBabar <71889240+FarhanZaheerBabar@users.noreply.github.com>
2020-09-27 17:41:18 +0500

    second way to import the file

--- loop.py
+++ loop.py
@@ -1,6 +1,13 @@
-from function import square
+#way one to import the second file
+# from function import square
+
+#way two to import the second file
+import function
 
 #forloop
 for i in range(10):
-    #formated String
-    print(f"the square of {i} is {square(i)}")
+    #formated String way one to import the second file
+    #print(f"the square of {i} is {square(i)}")
+
+    #formated String way two to import the second file
+    print(f"the square of {i} is {function.square(i)}")
