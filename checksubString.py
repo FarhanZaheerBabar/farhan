51e8c5c79225ac3bcc70e1b59059d4cdfaef548e
FarhanZaheerBabar <71889240+FarhanZaheerBabar@users.noreply.github.com>
2020-10-01 08:07:54 +0500

    successfuly retrive all results

--- checksubString.py
+++ checksubString.py
@@ -7,12 +7,13 @@
 result = []
 array_contain_strings = ["my","my name","my name is","my name is farhan","i","i live","i live in","i live in lahore","govt","govt central","govt central model","govt central model school","long","long live","long live pakistan"]
 final_str = ""
+last_index_list = ""
 length = len(array_contain_strings)
 i = 0
 while i < length:
     temp = i + 1
     print(temp)
-    if temp < 15:
+    if temp < length:
         if array_contain_strings[i] in array_contain_strings[temp]:
             if len(array_contain_strings[temp]) > len(array_contain_strings[i]):
                 final_str = array_contain_strings[temp]
@@ -20,7 +21,11 @@ while i < length:
         else:
             result.append(final_str)
             print('false')
+    else:
+        last_index_list = array_contain_strings[-1]
+        result.append(last_index_list)
     i = i + 1
+    
 
 for child in result:
     print(child)
